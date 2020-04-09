8b39124b11951e17ca0813ad8c8a3c832b29a8a8
Geovane Jr <geovane.a.f.junior@gmail.com>
2020-04-09 15:40:18 -0300

    subindo aprovação

--- app/Models/DataAux.php
+++ app/Models/DataAux.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 /**
  * @method static create(array $data)
  * @method static find($id)
+ * @method static where(string $string, string $string1, $id)
  */
 class DataAux extends Model
 {
